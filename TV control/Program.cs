7a329dc2765bc04bc22d31b216832c20c1ebbff0
Razieh Barzegar <Razieh.Brz.dev@gmail.com>
2023-12-02 11:37:31 +0330

    ...

---
+++ TV control/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        var isLove = false;
+        int i = 0;
+
+        do
+        {
+            i++;
+            Console.Clear();
+            Console.WriteLine($"**CHANNEL {i}**");
+            Console.WriteLine("Do you love this channel? (yes/no)");
+
+            var response = Console.ReadLine().ToLower();
+            isLove = response == "yes";
+        } while (!isLove);
+
+        Console.WriteLine("Enjoy watching CHANNEL " + i);
+        Console.WriteLine("^_^");
+    }
+}
