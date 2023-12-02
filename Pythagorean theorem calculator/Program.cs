afdfda4e4a825182a5f48040f9f091e2c71fd8c8
Razieh Barzegar <Razieh.Brz.dev@gmail.com>
2023-12-02 10:26:18 +0330

    ...

---
+++ Pythagorean theorem calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+
+        Console.WriteLine("Enter side A:");
+        var sideA = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Enter side B:");
+        var sideB = Convert.ToDouble(Console.ReadLine());
+
+        var result = calculator(sideA, sideB);
+        Console.WriteLine($"The result is: {result}");
+
+    }
+    static double calculator(double a, double b)
+    {
+        return Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+    }
+}
