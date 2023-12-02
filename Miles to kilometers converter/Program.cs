a3bb2a001f6c13c7374b6f3ee9588ff9fd5777de
Razieh Barzegar <Razieh.Brz.dev@gmail.com>
2023-12-02 10:11:42 +0330

    ...

---
+++ Miles to kilometers converter/Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Clear();
+
+        Console.WriteLine("\nEnter the distance in miles :");
+        var miles = Convert.ToDouble(Console.ReadLine());
+
+        var result = milesToKilometersConverter(miles);
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine($"\n\n --> {miles} miles is equal to {result} kilometers. ");
+        Console.ResetColor();
+    }
+    static double milesToKilometersConverter(double miles)
+    {
+        //1 mile = 1.60934 kilometers
+        const double constValue = 1.60934;
+        return miles * constValue;
+    }
+}
