e161fc414d4d1a1feb27e3f261de52a4096983d1
Razieh Barzegar <Razieh.Brz.dev@gmail.com>
2023-12-14 10:55:54 +0330

    calculator

---
+++ Calculator/Calculator.cs
@@ -0,0 +1,41 @@
+using System.Runtime.Intrinsics.Arm;
+
+public class Calculator
+{
+    public double FirstNumber { get; set; }
+    public double SecondNumber { get; set; }
+    public char _operator;
+    public double _result;
+
+    public char PrintMenu()
+    {
+        Console.WriteLine("Options:");
+        Console.WriteLine("\t+ : Add");
+        Console.WriteLine("\t- : Substract");
+        Console.WriteLine("\t* : Multiply");
+        Console.WriteLine("\t/ : Divide");
+        Console.Write("Enter an option: ");
+        return _operator = Convert.ToChar(Console.ReadLine());
+    }
+    public void CalculateResult()
+    {
+        switch (_operator)
+        {
+            case '+':
+                _result = FirstNumber + SecondNumber;
+                break;
+            case '-':
+                _result = FirstNumber - SecondNumber;
+                break;
+            case '*':
+                _result = FirstNumber * SecondNumber;
+                break;
+            case '/':
+                _result = FirstNumber / SecondNumber;
+                break;
+            default:
+                throw new Exception("You entered valid option!");
+        }
+        Console.WriteLine($"Your result: {FirstNumber} {_operator} {SecondNumber} = {_result}");
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,40 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        var calculator = new Calculator();
+        bool runAgain = false;
+
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("------------------");
+            Console.WriteLine("Calculator Program");
+            Console.WriteLine("------------------");
+
+            try
+            {
+                getNumbers(calculator);
+                calculator.PrintMenu();
+                calculator.CalculateResult();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine("Would you like to continue? (Y = yes/N = no)");
+            var response = Console.ReadLine();
+            runAgain = response.ToUpper() == "Y";
+
+        } while (runAgain);
+
+        Console.WriteLine("Bye :)");
+    }
+    static void getNumbers(Calculator calculator)
+    {
+        Console.Write("Enter number 1: ");
+        calculator.FirstNumber = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Enter number 2: ");
+        calculator.SecondNumber = Convert.ToDouble(Console.ReadLine());
+    }
+}
